3fc08fb6bfed33a35d35f1d36cd363f49f87f027
Oscar Mera <odmera77@gmail.com>
2019-05-06 10:53:03 -0500

    Update ConsumidorHttpClienteImp.java

--- ConsumidorHttpClienteImp.java
+++ ConsumidorHttpClienteImp.java
@@ -1,4 +1,4 @@
-package co.com.suramericana.saludweb.spring.utilidades.servicio.imp;
+package mipaquete.spring.utilidades.servicio.imp;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import co.com.suramericana.saludweb.spring.utilidades.servicio.ConsumidorHttpClienteInt;
 
 /**<b>Descripci&oacute;n:</b><br>
 * Esta clase implementa los diferentes funcionalidades necesarias para realizar peticiones http
